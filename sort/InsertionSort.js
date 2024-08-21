d2d676e64dc2b66f9c8bce1ef4e7f4562b7c20c5
aoi-oota <145953201+aoi-oota@users.noreply.github.com>
2024-08-22 03:47:45 +0900

    挿入ソートです

---
+++ sort/InsertionSort.js
@@ -0,0 +1,25 @@
+function swap(arr, a, b) {
+    var tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+}
+
+function InsertionSort(arr) {
+    let len = arr.length;
+    let holder;
+
+    for (let i = 1; i < len; i++) {
+        holder = arr[i];
+
+        for (let j = i - 1; j >= 0; j--) {
+            if (a[j] > holder) {
+                arr[j + 1] = arr[j];
+            }
+            else {
+                break;
+            }
+        }
+        arr[j + 1] = holder;
+    }
+    return arr;
+}
