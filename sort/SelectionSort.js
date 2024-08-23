b83c1bf6ce801d68ed19614b68000179402c4ca9
aoi-oota <145953201+aoi-oota@users.noreply.github.com>
2024-08-24 02:25:32 +0900

    1~9を適当に配置した配列で試してるんだけどbiggestが7以降を捕まえられてない、多分

--- sort/SelectionSort.js
+++ sort/SelectionSort.js
@@ -1,27 +1,22 @@
-function SelectionSort(arr)
-{
-	const len = arr.length;
-	let biggest;
-	let holder;
+function SelectionSort(arr) {
+    const len = arr.length;
+    let biggest;
+    let holder;
 
-	for (let i = 0; i < len; i++)
-	{
-		biggest = i;
+    for (let i = 0; i < len; i++) {
+        biggest = i;
 
-		for (let j = i; j < len; j++)
-		{
-			if (arr[j] < arr[j + 1])
-			{
-				biggest = j;
-			}
-		}
-		holder = arr[biggest];
-		
-		for(let k = biggest - 1; k >= i; k--)
-		{
-			arr[k + 1] = arr[k];
-		}
-	}
-
-	return arr;
+        for (let j = i; j < len; j++) {
+            if (arr[j] < arr[j + 1]) {
+                biggest = j + 1;
+            }
+        }
+        holder = arr[biggest];
+        for (let k = biggest - 1; k >= i; k--) {
+            arr[k + 1] = arr[k];
+        }
+        arr[i] = holder;
+    }
+    console.log(arr);
+    return arr;
 }
